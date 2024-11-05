7509059914171098b6a1b5c24c38f0c34ff7708b
LimonHog <153211989+LimonHog@users.noreply.github.com>
2024-11-05 19:34:30 +0300

    добавил check_for_redirect()

--- books_downloader.py
+++ books_downloader.py
@@ -10,20 +10,25 @@ import os
 # with open(filename, 'wb') as file:
 #     file.write(response.content)
 
+if os.path.exists('books') == False:
+    os.mkdir('books')
 
 
-    
+def check_for_redirect(response):    
+    if response.history:
+        raise requests.HTTPError
 
 
-for i in range(10):
-    i += 1
+for i in range(1, 11):
 
     url = f"https://tululu.org/txt.php?id={i}"
-    response = requests.get(url)
-    response.raise_for_status() 
+    try:
+        response = requests.get(url)
+        response.raise_for_status() 
+        check_for_redirect(response)
 
-    filename = f'books/id{i}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-    
-    
+        filename = f'books/id{i}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+    except requests.HTTPError:
+        print("Встречена ошибка requests.HTTPError")
