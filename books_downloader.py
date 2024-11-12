e08f968aad6eacdd2416645593d43d0046300d18
LimonHog <153211989+LimonHog@users.noreply.github.com>
2024-11-12 18:48:36 +0300

    Сделал вывод названия и автора первой книги

--- books_downloader.py
+++ books_downloader.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
 # url = "https://tululu.org/txt.php?id=32168"
 # response = requests.get(url)
@@ -19,16 +20,26 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-for i in range(1, 11):
+# for i in range(1, 11):
 
-    url = f"https://tululu.org/txt.php?id={i}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status() 
-        check_for_redirect(response)
+#     url = f"https://tululu.org/txt.php?id={i}"
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status() 
+#         check_for_redirect(response)
 
-        filename = f'books/id{i}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-    except requests.HTTPError:
-        print("Встречена ошибка requests.HTTPError")
+#         filename = f'books/id{i}.txt'
+#         with open(filename, 'wb') as file:
+#             file.write(response.content)
+#     except requests.HTTPError:
+#         print("Встречена ошибка requests.HTTPError")
+
+
+url = 'http://tululu.org/b1/'
+response = requests.get(url)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, 'lxml')
+id1_title = soup.find(id='content').find('h1').text
+id1_title = id1_title.split('::')
+print("Название:", id1_title[0],  "\\\nАвтор:", id1_title[1])
